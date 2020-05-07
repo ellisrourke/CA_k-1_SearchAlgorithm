8da55b25c10d9b3a3ab349c7fd98b92f3a396539
ellisrourkePC <ellisrourke@gmail.com>
2020-05-07 18:04:33 +1000

    output path

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <unordered_map>
 #include <fstream>
 #include <vector>
+#include <algorithm>
+
 class ksp{
 public:
     void readData(const std::string &fileName){
@@ -15,8 +17,6 @@ public:
         agencyMatrix.resize(n);
         for (int i = 0; i < n; ++i){agencyMatrix[i].resize(n);}
 
-
-
         int a=0,b=0;
         double w=0.0;
         int count =0;
@@ -28,8 +28,6 @@ public:
         }
         std::getline(inFile,inLine);
         inFile >> source >> dest;
-        //outputMatrix();
-        dijkstra(source);
     }
     void outputMatrix(){
         std::cout << "  ";
@@ -43,7 +41,6 @@ public:
             std::cout<< std::endl;
         }
     }
-
     int minDist(std::vector<double> &dist, std::vector<bool> &visitedList){
         int min = INT_MAX;
         int index;
@@ -57,38 +54,33 @@ public:
         return index;
     }
 
-    static void printPath(std::vector<int> &parent, int &j)
+    /*void getPath(std::vector<int> &parent, int &j)
     {
-
         // Base Case : If j is source
         if (parent[j] == - 1)
             return;
-        printPath(parent, parent[j]);
+        edgeList.emplace_back(std::make_tuple(parent[j],j));
+        getPath(parent, parent[j]);
+        //std::cout << j << " ";
+    }*/
 
-        std::cout << j;
-    }
-
-    int printSolution(std::vector<double> &dist, std::vector<int> &parent)
-    {
-        int src = source;
-        printf("Vertex\t Distance\tPath\n");
-        for (int i = dest; i <= dest; i++)
-        {
-            if(dist[i] == INT_MAX or dist[i] <0){
-                std::cout << src << " -> "  << i << "\t " << "null\t\t";
-
-            } else {
-                std::cout << src << " -> "  << i <<  "\t " << dist[i] << "\t\t";
-            }
-            //printf("\n%d -> %d \t\t %d\t\t%d ",src, i, dist[i], src);
-            //printPath(parent, i);
-            std::cout << std::endl;
-
+    void kSolution(std::vector<int> &parent, int j){
+        while(j!=-1){
+            path.emplace_back(parent[j]);
+            //std::cout << j;
+            j = parent[j];
         }
-    }
+        std::cout << dest << std::endl;
+        for(int i=0;i<path.size()-1;i++){
+            std::cout << path[i] << std::endl;
+        }
+        //dijkstra(source,1);
+        }
 
-    void dijkstra(int src){
-        std::vector<double>dist(n);
+
+
+    double dijkstra(int src,int count=0){
+        std::vector<double> dist(n);
         std::vector<int> parent(n);
         std::vector<bool>visited(n,false);
         parent[0] = -1;
@@ -99,30 +91,40 @@ public:
         for(int i =0; i<n-1;i++){ //find next closest node
             int nextNode = minDist(dist,visited);
             visited[nextNode] = true;
-            //std::cout << nextNode <<std::endl;
             for(int j=0;j<n;j++){ //update distances for next nodes
-
                 if (!visited[j] && agencyMatrix[nextNode][j]!=0 && dist[nextNode] + agencyMatrix[nextNode][j] < dist[j]){
                     parent[j] = nextNode;
                     dist[j] = dist[nextNode] + agencyMatrix[nextNode][j];
                 }
             }
         }
-        printSolution(dist,parent);
-
+        //if(called==0)
+            //getPath(parent,dist[dest]
+            std::cout << "distance = " << dist[dest] << std::endl;
+            kSolution(parent,dest);
+        //..if(k>1 && called==0) {
+         //   ..kSoultion(dist,parent);
+        //}
+        //k -= 1;
 
     }
+
+public:
+    int n,m; //num nodes, num edges
+    int source,dest;
+    int k = 1; //source dest kTimes
 protected:
     std::vector<std::vector<double>> agencyMatrix;
     std::vector<int> visited;
-    int n,m; //num nodes, num edges
-    int source,dest; //source dest
+    std::vector<int> path;
+
 };
 
 
 int main(int argc, char *argv[]) {
     ksp main;
     main.readData(argv[1]);
+    main.dijkstra(main.source);
 
 
  //N = number of veriticies
